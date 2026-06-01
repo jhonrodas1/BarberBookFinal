6ad3731864bd94a88b9885d10957615ca86be4f4
julianisazam <julian.isazam@udea.edu.co>
2026-06-01 18:02:56 -0500

    fix: update API URL to new Render deployment

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,1 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api':{
-        target: 'http://localhost:8080',
-        changeOrigin: true
-      }
-    }
-  }
-})
+const BASE = import.meta.env.VITE_API_URL || '/api'
