60f7f94ea6f12ab17109faee197292c0532b0fda
julianisazam <julian.isazam@udea.edu.co>
2026-06-02 12:03:34 -0500

    link vite url fixed

--- src/api/client.js
+++ src/api/client.js
@@ -1,4 +1,4 @@
-const BASE = import.meta.env.VITE_API_URL || '/api'
+const BASE = import.meta.env.VITE_API_URL ||'https://barbershop-mh6w.onrender.com/api'
 
 export async function apiFetch(ruta, opciones = {}) {
   const token = localStorage.getItem('token')
